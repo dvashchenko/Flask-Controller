7c959759d7a38e730e723d3054a82c1c9594d55b
Denis Vashchenko <denisvashchenko@gmail.com>
2018-10-23 14:17:46 -0700

    finished user input

--- userCode/code.py
+++ userCode/code.py
@@ -1,24 +1,11 @@
 x = raw_input()
 
-print x 
+print x
 
 y = raw_input()
 
-print y 
+print y
 
 z = raw_input()
 
-print z 
-
-a = raw_input()
-
-print a
-
-b = raw_input()
-
-print b 
-
-c = raw_input()
-
-print c 
-
+print z

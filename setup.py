29f9b7cada0c0b5a30a5a2b74b957500ed356e39
Denis Vashchenko <denisvashchenko@gmail.com>
2019-02-17 19:54:25 -0800

    Packaged app.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='Needle',
     version='1.0.0',
     packages=['needle'],
-    install_require=['flask','flask-bootstrap','flask-wtf','flask-codemirror'],
+    install_requires=['flask>=1.0.2','flask-bootstrap','flask-wtf','flask-codemirror'],
     license='MIT',
     long_description=open('README.md').read(),
-    author='Denis Vashchenko',
+    author='Denis Vashchenko'
 )
 
